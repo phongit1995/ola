6d04e12126cb9b16627b27e0f444f9947c527c95
phong <duc.phan33@ntq-solution.com.vn>
2026-07-23 21:01:00 +0700

    update code

--- game/caro/assets.ts
+++ game/caro/assets.ts
@@ -88,7 +88,7 @@ import boardTurnLeft from './assets/board/turn-left.png';
 import boardTurnRight from './assets/board/turn-right.png';
 import boardAvatarFrame from './assets/board/avatar-frame.png';
 import boardMenuBtn from './assets/board/menu-btn.png';
-import boardChatFrame from './assets/board/chat-frame.png';
+import boardChatFrame from './assets/board/chat-frame-clean.png';
 import boardSendIcon from './assets/board/send-icon.png';
 import leaderboardTitleFrame from './assets/leaderboard/title-frame.webp';
 import leaderboardCup from './assets/leaderboard/cup.webp';
