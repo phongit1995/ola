ca92cbf9fad6628ea1f5ca51f9da8a568f4778df
phong <duc.phan33@ntq-solution.com.vn>
2026-08-19 21:57:34 +0700

    fix(chat): scope contact icon change to web

--- mobile/src/screens/chat/components/ContactsPane.tsx
+++ mobile/src/screens/chat/components/ContactsPane.tsx
@@ -37,7 +37,6 @@ import { DIVIDER } from '@constants';
 
 const snapPicIcon = require('@assets/icons/chat/icon_snap_pic.png');
 const addFriendIcon = require('@assets/icons/room/ic_add_friend.png');
-const addContactIcon = require('@assets/icons/chat/ic_add_contact_gray.png');
 const searchIcon = require('@assets/icons/chat/ic_search_gray.png');
 
 function ActionRow({
@@ -375,7 +374,7 @@ export function ContactsPane({ onAccountMenu }: { onAccountMenu?: () => void }) 
           shadowOffset: { width: 0, height: 3 },
         }}
       >
-        <Image source={addContactIcon} style={{ width: 24, height: 24, tintColor: '#fff' }} resizeMode="contain" />
+        <Image source={addFriendIcon} style={{ width: 24, height: 24, tintColor: '#fff' }} resizeMode="contain" />
       </Pressable>
 
       <MessageActionSheet
